33f0fe816927d90c1c529a81069bc9ad36f84928
angelica-thd <ageliki.th@gmail.com>
2021-01-19 12:28:32 +0200

    configured routes and fixed shoulda::matchers error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  resources :todos do
+    resources :items
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
