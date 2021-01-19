8b6b5c90257c1051f93253dd2a60c58277cd8195
angelica-thd <ageliki.th@gmail.com>
2021-01-19 12:46:34 +0200

    added exception handler and json exception reader

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,7 @@
 class TodosController < ApplicationController
-  before_action :set_todo, only: [:show, :update, :destroy]
-
-  # GET /todos
-  def index
-    @todos = Todo.all
-    json_response(@todos)
+  # POST /todos
+  def create
+    @todo = Todo.create!(todo_params)
+    json_response(@todo, :created)
   end
-
 end
