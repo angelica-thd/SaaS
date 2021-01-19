eb6932dd2835c070a90030ea99935ec70ae2acf3
angelica-thd <ageliki.th@gmail.com>
2021-01-19 12:41:32 +0200

    added GET/todos with its test_request

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,10 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+  # GET /todos
+  def index
+    @todos = Todo.all
+    json_response(@todos)
+  end
+
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,16 +40,10 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-RSpec.configuration do |config|
-  # [...]
-  config.include RequestSpecHelper, type: :request
-  # [...]
-end
-
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  config.include RequestSpecHelper, type: :request
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
