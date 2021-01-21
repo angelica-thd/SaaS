f8a9e8ffac70aeea7f6c8c4243f1e49f9235c339
angelica-thd <ageliki.th@gmail.com>
2021-01-21 12:08:49 +0200

    added auth\login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  post 'auth/login', to: 'authentication#authenticate'
   resources :todos do
     resources :items
   end
